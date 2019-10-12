420a787c73a449c810d11325869c3fa9b756d298
ashaduzzamane <enan@live.ca>
2019-10-12 12:59:51 -0400

    small changes to PoolStats

--- src/client/components/PoolStats.jsx
+++ src/client/components/PoolStats.jsx
@@ -7,51 +7,51 @@ class PoolStats extends Component {
     render() {
         return (
             <section id="PoolStats">
-        <p class="sectionHeader">POOL STATS</p>
-        <div class="graphs">
-            <div class="firstRow">
-                <div class="card" style="width: 18rem;">
-                    <img src="screenShot.png" class="card-img-top" alt="..." />
-                    <div class="card-body">
-                        <h3>50.98 <span class="cardSubText">MH/sec</span></h3>
-                        <p class="card-text">Pool Hash Rate</p>
+        <p className={Style.sectionHeader}>POOL STATS</p>
+        <div className={Style.graphs}>
+            <div className={Style.firstRow}>
+                <div className={Style.card} style="width: 18rem;">
+                    <img src="screenShot.png" className="card-img-top" alt="..." />
+                    <div className={cardBody}>
+                        <h3>50.98 <span className={Style.cardSubText}>MH/sec</span></h3>
+                        <p className="card-text">Pool Hash Rate</p>
                     </div>
                 </div>
-                <div class="card" style="width: 18rem;">
-                    <img src="screenShot.png" class="card-img-top" alt="..." />
-                    <div class="card-body">
-                        <h3>22 <span class="cardSubText">Minutes</span></h3>
-                        <p class="card-text">Previous Block Found</p>
+                <div className={Style.card} style="width: 18rem;">
+                    <img src="screenShot.png" className="card-img-top" alt="..." />
+                    <div className={Style.cardBody}>
+                        <h3>22 <span className={Style.cardSubText}>Minutes</span></h3>
+                        <p className="card-text">Previous Block Found</p>
                     </div>
                 </div>
-                <div class="card" style="width: 18rem;">
-                    <img src="screenShot.png" class="card-img-top" alt="..." />
-                    <div class="card-body">
-                        <h3>9,477 <span class="cardSubText"></span></h3>
-                        <p class="card-text">Miners Connected</p>
+                <div className={Style.card} style="width: 18rem;">
+                    <img src="screenShot.png" className="card-img-top" alt="..." />
+                    <div className={Style.cardBody}>
+                        <h3>9,477 <span className={Style.cardSubText}></span></h3>
+                        <p className="card-text">Miners Connected</p>
                     </div>
                 </div>
             </div>
             <div class="secondRow">
-                <div class="card" style="width: 18rem;">
-                    <img src="screenShot.png" class="card-img-top" alt="..." />
-                    <div class="card-body">
-                        <h3>333.94 <span class="cardSubText">MH/sec</span></h3>
-                        <p class="card-text">Network Hash Rate</p>
+                <div className={Style.card} style="width: 18rem;">
+                    <img src="screenShot.png" className="card-img-top" alt="..." />
+                    <div className={Style.cardBody}>
+                        <h3>333.94 <span className={Style.cardSubText}>MH/sec</span></h3>
+                        <p className="card-text">Network Hash Rate</p>
                     </div>
                 </div>
-                <div class="card" style="width: 18rem;">
-                    <img src="screenShot.png" class="card-img-top" alt="..." />
-                    <div class="card-body">
-                        <h3>2.4041 <span class="cardSubText">XMR</span></h3>
-                        <p class="card-text">Block Reward</p>
+                <div className={Style.card} style="width: 18rem;">
+                    <img src="screenShot.png" className="card-img-top" alt="..." />
+                    <div className={Style.cardBody}>
+                        <h3>2.4041 <span className={Style.cardSubText}>XMR</span></h3>
+                        <p className="card-text">Block Reward</p>
                     </div>
                 </div>
-                <div class="card" style="width: 18rem;">
-                    <img src="screenShot.png" class="card-img-top" alt="..." />
-                    <div class="card-body">
-                        <h3>$70.60 <span class="cardSubText">USD</span></h3>
-                        <p class="card-text">XMR Value</p>
+                <div className={Style.card} style="width: 18rem;">
+                    <img src="screenShot.png" className="card-img-top" alt="..." />
+                    <div className={Style.cardBody}>
+                        <h3>$70.60 <span className={Style.cardSubText}>USD</span></h3>
+                        <p className="card-text">XMR Value</p>
                     </div>
                 </div>
             </div>
