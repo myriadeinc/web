6997c3e1ce88778bed6f3b43abc111a7f3b7bb59
Jonathan <jonathanxu25@gmail.com>
2019-12-01 22:25:46 -0500

    updated landing page alert color

--- src/client/pages/LandingPage.jsx
+++ src/client/pages/LandingPage.jsx
@@ -4,12 +4,14 @@ import LandingBody from '../components/landing/LandingBody.jsx';
 
 import { Alert } from 'shards-react';
 
+import PageStyle from '../styles/pages/Landing.less';
+
 class LandingPage extends Component {
 
   render() {
     return (
       <>
-        <Alert theme="warning"> Our mining pool is currently under Beta testing and not fully operational yet. </Alert>
+        <Alert theme="warning" className={PageStyle.amber}> Our mining pool is currently under Beta testing and not fully operational yet. </Alert>
         <LandingBody />
       </>
     );
