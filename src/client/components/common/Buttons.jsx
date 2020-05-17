6cca09c6f3873b9a17c38b8763cbde024ced796c
Jonathan <jonathanxu25@gmail.com>
2020-05-17 19:15:28 -0400

    fixing alert bug and updated buttons

--- src/client/components/common/Buttons.jsx
+++ src/client/components/common/Buttons.jsx
@@ -1,17 +1,17 @@
-import { Button } from 'shards-react';
-import styled from 'styled-components';
+import { Button } from "shards-react";
+import styled from "styled-components";
 
 export const PrimaryButton = styled(Button)`
   border-radius: 100rem;
   padding: 1rem;
   font-size: 1rem;
-  padding: .5rem 3rem;
+  padding: 0.5rem 3rem;
   border-radius: 25px;
   margin: 2px;
   color: #fff;
   border: solid 3px transparent;
   background-image: linear-gradient(to right, #cc4b00 0%, #ffa600 100%);
-  &:hover { 
+  &:hover {
     background-position: right center;
   }
 `;
@@ -24,17 +24,43 @@ export const SecondaryButton = styled(Button)`
   box-shadow: 0 0 6px 0 rgba(157, 96, 212, 0.5);
   border: solid 3px transparent;
   color: #003cbe;
-  background-image: linear-gradient(to right, #3e11e2 0%, #0300be 55%, #0050fc 100%);
+  background-image: linear-gradient(
+    to right,
+    #3e11e2 0%,
+    #0300be 55%,
+    #0050fc 100%
+  );
   &:hover {
-    background-image: linear-gradient(to right, #3e11e2 0%, #0300be 55%, #0050fc 100%);
+    background-image: linear-gradient(
+      to right,
+      #3e11e2 0%,
+      #0300be 55%,
+      #0050fc 100%
+    );
     color: white;
   }
 `;
 
-export const PrimaryButtonOutline = styled(PrimaryButton)`
-  border: solid 3px white !important;
+export const LightButton = styled(Button)`
+  border-radius: 100rem;
+  padding: 1rem;
+  font-size: 1rem;
+  padding: 0.5rem 3rem;
+  border-radius: 25px;
+  margin: 2px;
+  color: #f07c00 !important;
+  background-color: white !important;
+  border-width: 0 !important;
 `;
 
-export const SecondaryButtonOutline = styled(SecondaryButton)`
-  border: solid 3px white !important;
+export const LightSecondaryButton = styled(Button)`
+  border-radius: 100rem;
+  padding: 1rem;
+  font-size: 1rem;
+  padding: 0.5rem 3rem;
+  border-radius: 25px;
+  margin: 2px;
+  color: white !important;
+  background-color: #f07c00 !important;
+  border-width: 0 !important;
 `;
