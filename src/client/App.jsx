c88a9097751e970e41a1652672f682ef8e8ff4b9
Jonathan <jonathanxu25@gmail.com>
2020-05-19 23:34:50 -0400

    adding frontend for password reset

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -1,27 +1,27 @@
 import React from "react";
-import { withRouter, Route, Redirect, NavLink, Switch } from 'react-router-dom';
+import { withRouter, Route, Redirect, NavLink, Switch } from "react-router-dom";
 
-import * as ROUTES from './utils/routes.js'
+import * as ROUTES from "./utils/routes.js";
 
-import { AuthConsumer, ProtectedRoute } from './layers/AuthLayer.jsx';
+import { AuthConsumer, ProtectedRoute } from "./layers/AuthLayer.jsx";
 
-import DashboardPage from './pages/Dashboard.jsx';
-import LoginPage from './pages/LoginPage.jsx';
-import LandingPage from './pages/LandingPage.jsx';
-import SignupPage from './pages/SignupPage.jsx';
-import NavigationBar from './components/common/Navbar.jsx';
-import CookiesToast from './components/common/CookiesToast.jsx';
+import DashboardPage from "./pages/Dashboard.jsx";
+import LoginPage from "./pages/LoginPage.jsx";
+import LandingPage from "./pages/LandingPage.jsx";
+import SignupPage from "./pages/SignupPage.jsx";
+import PasswordForget from "./pages/PasswordForget.jsx";
+import NavigationBar from "./components/common/Navbar.jsx";
+import CookiesToast from "./components/common/CookiesToast.jsx";
 
-import Cookies from './components/landing/CookiesPolicy.jsx';
-import Privacy from './components/landing/PrivacyPolicy.jsx';
-import Terms from './components/landing/TermsOfService.jsx';
-import LinuxInstructions from './components/landing/LinuxInstructions.jsx';
+import Cookies from "./components/landing/CookiesPolicy.jsx";
+import Privacy from "./components/landing/PrivacyPolicy.jsx";
+import Terms from "./components/landing/TermsOfService.jsx";
+import LinuxInstructions from "./components/landing/LinuxInstructions.jsx";
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './styles/components/common/Theme.less';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./styles/components/common/Theme.less";
 
 class App extends React.Component {
-
   render() {
     return (
       <div>
@@ -31,8 +31,17 @@ class App extends React.Component {
               <NavigationBar />
               <Switch>
                 <Route exact path={ROUTES.LANDING} component={LandingPage} />
-                <Route path={ROUTES.LOGIN} component={() => (<LoginPage login={login} authenticated={authenticated} />)} />
+                <Route
+                  path={ROUTES.LOGIN}
+                  component={() => (
+                    <LoginPage login={login} authenticated={authenticated} />
+                  )}
+                />
                 <Route path={ROUTES.SIGN_UP} component={SignupPage} />
+                <Route
+                  path={ROUTES.PASSWORD_FORGET}
+                  component={PasswordForget}
+                />
 
                 <Route path={ROUTES.COOKIES} component={Cookies} />
                 <Route path={ROUTES.PRIVACY} component={Privacy} />
@@ -41,14 +50,18 @@ class App extends React.Component {
                 <Route path={ROUTES.LINUX} component={LinuxInstructions} />
 
                 {/* Protected Routes that need authentication */}
-                <ProtectedRoute path={ROUTES.DASHBOARD} component={DashboardPage} authenticated={authenticated} />
+                <ProtectedRoute
+                  path={ROUTES.DASHBOARD}
+                  component={DashboardPage}
+                  authenticated={authenticated}
+                />
               </Switch>
               <CookiesToast />
             </>
           )}
         </AuthConsumer>
       </div>
-    )
+    );
   }
 }
 
--- src/client/utils/config.js
+++ src/client/utils/config.js
@@ -1,8 +1,7 @@
-
 const config = {
-  identity_service_url: 'https://staging.myriade.io/id',
-  miner_metrics_url: 'https://staging.myriade.io/metrics',
-  raffles_url: 'https://onyx-alpha.herokuapp.com'
-}
+  identity_service_url: "https://staging.myriade.io/id", //diamond
+  miner_metrics_url: "https://staging.myriade.io/metrics", //sapphire
+  raffles_url: "https://onyx-alpha.herokuapp.com", //onyx
+};
 
 export default config;
