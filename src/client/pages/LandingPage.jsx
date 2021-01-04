24999c611c4a72d0b4ab82ba3b07ef8aa22761cc
unknown <bfengos@gmail.com>
2021-01-04 02:39:52 -0500

    Changed landing page banner to read alpha - ALPHA-37

--- src/client/pages/LandingPage.jsx
+++ src/client/pages/LandingPage.jsx
@@ -1,37 +1,43 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 
-import LandingBody from '../components/landing/LandingBody.jsx';
-import LandingFooter from '../components/landing/LandingFooter.jsx';
+import LandingBody from '../components/landing/LandingBody.jsx'
+import LandingFooter from '../components/landing/LandingFooter.jsx'
 
-import { Alert } from 'shards-react';
+import { Alert } from 'shards-react'
 
-import PageStyle from '../styles/pages/Landing.less';
+import PageStyle from '../styles/pages/Landing.less'
 
 class LandingPage extends Component {
-  constructor(props) {
-    super(props);
+    constructor(props) {
+        super(props)
 
-    this.dismiss = this.dismiss.bind(this);
-    this.state = {
-      poolAlert: true
+        this.dismiss = this.dismiss.bind(this)
+        this.state = {
+            poolAlert: true,
+        }
     }
-  }
 
-  dismiss() {
-    this.setState({ poolAlert: false })
-  }
+    dismiss() {
+        this.setState({ poolAlert: false })
+    }
 
-  render() {
-    return (
-      <>
-        <Alert theme="warning" className={PageStyle.amber} dismissible={this.dismiss} open={this.state.poolAlert}>
-          Our Mining Pool is in beta so there may be some issues. Thanks for your patience!
-        </Alert>
-        <LandingBody />
-        <LandingFooter />
-      </>
-    );
-  }
+    render() {
+        return (
+            <>
+                <Alert
+                    theme="warning"
+                    className={PageStyle.amber}
+                    dismissible={this.dismiss}
+                    open={this.state.poolAlert}
+                >
+                    Our Mining Pool is in alpha so there may be some issues.
+                    Thanks for your patience!
+                </Alert>
+                <LandingBody />
+                <LandingFooter />
+            </>
+        )
+    }
 }
 
-export default LandingPage;
+export default LandingPage
