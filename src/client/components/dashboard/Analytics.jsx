cf76aade9bcf92321dae011e9fbfd3029f80b913
unknown <bfengos@gmail.com>
2021-03-04 02:46:22 -0500

    fixed pool rate card on dashboard

--- src/client/components/dashboard/Analytics.jsx
+++ src/client/components/dashboard/Analytics.jsx
@@ -8,7 +8,23 @@ import Style from '../../styles/components/dashboard/Analytics.less';
 
 import _ from 'lodash';
 
+import axios from 'axios';
+import config from '../../utils/config.js';
+
 class AnalyticsComponent extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      poolRate: 0,
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get(`${config.miner_metrics_url}/v1/public/poolInfo`)
+      .then((res) => this.setState({ poolRate: res.data.hashrate }));
+  }
+
   render() {
     let width = window.innerWidth;
 
@@ -59,7 +75,9 @@ class AnalyticsComponent extends Component {
                   <Card.ImgOverlay className={Style.CardOverlay + ' p-3'}>
                     <div>
                       <h1 className={Style.dynamicSubtitle}>Pool Hashrate</h1>
-                      <h1 className={Style.dynamicTitle}>N/A</h1>
+                      <h1 className={Style.dynamicTitle}>
+                        {`${this.state.poolRate} H/sec`}
+                      </h1>
                     </div>
                   </Card.ImgOverlay>
                 </Card>
@@ -70,7 +88,7 @@ class AnalyticsComponent extends Component {
                   <Card.Text>
                     Your average hashrate: {miner.average_hashrate}
                   </Card.Text>
-                  <Card.Text>Pool hashrate: N/A</Card.Text>
+                  <Card.Text>Pool hashrate: {this.state.poolRate}</Card.Text>
                   <Card.Text>
                     Mining credits balance {miner.myriade_credits_balance}
                   </Card.Text>
