8ae1eda5a434a2c2d73a820c1d5d1130bac9acf2
hammad0860 <muhammad.h0860@gmail.com>
2025-01-29 22:40:15 -0500

    Created new homepage

---
+++ quartzHomepage/main.js
@@ -0,0 +1,30 @@
+/*const express = require('express');
+const path = require('path');
+const fs = require('fs');
+
+
+
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+
+app.use(express.static(path.join(__dirname, '.')));
+
+
+
+
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname,  'index.html'));
+});
+
+
+
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+  });
+
+
+
+*/
