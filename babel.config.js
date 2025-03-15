90a5b151f4d9f034c252c9b39ef2fc77967c40ab
hammad0860 <muhammad.h0860@gmail.com>
2025-03-15 18:02:12 -0400

    Update babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -1,14 +1,20 @@
 module.exports = function (api) {
     api.cache(true);
+  
     const presets = [
-        '@babel/preset-env',
-        '@babel/preset-react',
+      '@babel/preset-env',
+      '@babel/preset-react',
     ];
+  
     const plugins = [
-        '@babel/plugin-proposal-class-properties',
+      '@babel/plugin-proposal-class-properties',
+      '@babel/plugin-syntax-dynamic-import',
+      '@babel/plugin-transform-runtime',
     ];
+  
     return {
-        presets,
-        plugins,
+      presets,
+      plugins,
     };
-};
+  };
+  
