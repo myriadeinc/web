f12ad5af11dcdba79b2f841f1f6fabfcbf8a10f2
hammad0860 <muhammad.h0860@gmail.com>
2025-03-15 09:45:56 -0400

    Update LandingPage.jsx

--- src/client/pages/LandingPage.jsx
+++ src/client/pages/LandingPage.jsx
@@ -8,7 +8,7 @@ import "./assets/vendor/bootstrap/css/bootstrap.min.css";
 import "./assets/css/style.css";
 
 
-//import "./assets/vendor/aos/aos.css";
+import "./assets/vendor/aos/aos.css";
 import "./assets/vendor/bootstrap-icons/bootstrap-icons.css";
 import "./assets/vendor/boxicons/css/boxicons.min.css";
 import "./assets/vendor/glightbox/css/glightbox.min.css";
