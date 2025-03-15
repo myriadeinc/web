88648d81a6ca56a82f12ce917a7cfb2137419c1c
hammad0860 <muhammad.h0860@gmail.com>
2025-03-15 09:08:55 -0400

    Update LandingPage.jsx

--- src/client/pages/LandingPage.jsx
+++ src/client/pages/LandingPage.jsx
@@ -2,12 +2,18 @@ import React, { useRef, useEffect, useState } from "react";
 import WAVES from "./assets/js/vanta.waves.min";
 import * as THREE from "./assets/js/three.r119.min";
 
-
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
+
 import "./assets/vendor/bootstrap/css/bootstrap.min.css";
 import "./assets/css/style.css";
 
-function LandingPage() {
+import "./assets/vendor/aos/aos.css";
+import "./assets/vendor/bootstrap-icons/bootstrap-icons.css";
+import "./assets/vendor/boxicons/css/boxicons.min.css";
+import "./assets/vendor/glightbox/css/glightbox.min.css";
+import "./assets/vendor/remixicon/remixicon.css";
+import "./assets/vendor/swiper/swiper-bundle.min.css";
+
   const heroRef = useRef(null);
   const [vantaEffect, setVantaEffect] = useState(null);
 
