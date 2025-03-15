ae48790fab9417a2fb4ccb60c7833276c4ba2659
hammad0860 <muhammad.h0860@gmail.com>
2025-03-15 09:42:40 -0400

    Update LandingPage.jsx

--- src/client/pages/LandingPage.jsx
+++ src/client/pages/LandingPage.jsx
@@ -13,7 +13,7 @@ import "./assets/vendor/bootstrap-icons/bootstrap-icons.css";
 import "./assets/vendor/boxicons/css/boxicons.min.css";
 import "./assets/vendor/glightbox/css/glightbox.min.css";
 import "./assets/vendor/remixicon/remixicon.css";
-//import "./assets/vendor/swiper/swiper-bundle.min.css";
+import "./assets/vendor/swiper/swiper-bundle.min.css";
 
 function LandingPage() {
   const heroRef = useRef(null);
