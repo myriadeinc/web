15ef66d6120773e9b2438549a8d4768445f31731
hammad0860 <muhammad.h0860@gmail.com>
2025-03-21 09:44:45 -0400

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
   entry: './src/client/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'main.js'
+    filename: 'bundle.js'
   },
   module: {
     rules: [
