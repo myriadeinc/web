06b682f88f7ae098e2174726cc2367f3575b91dc
hammad0860 <muhammad.h0860@gmail.com>
2025-03-21 15:48:18 -0400

    deleted extra folder and changed webpack

--- webpack.config.js
+++ webpack.config.js
@@ -73,7 +73,6 @@ module.exports = {
     }),
     new CopyWebpackPlugin({
       patterns: [
-        { from: 'public/assets', to: 'assets' },
         { from: 'public/icons', to: 'icons' },
         { from: 'public/LandingPage', to: 'LandingPage' } 
 
